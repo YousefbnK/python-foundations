74901a25439a834f89eb08d331d2708495d6c5c3
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2019-12-19 14:51:46 +0300

    Basic Calculator

--- calculator.py
+++ calculator.py
@@ -10,13 +10,18 @@ def multiply(first_number, second_number):
 def divide(first_number, second_number):
 	return first_number / second_number
 
-try:
-	first_number = float(raw_input("Enter the first number  "))
-	second_number = float(raw_input("Enter the second number  "))
-except ValueError:
-	print("Invalid input, please enter number   ")
-else:
-	operation = raw_input("Choose the operation (+, -, /, *):  ")
+while True:
+	try:
+		first_number = float(input("Enter the first number  "))
+		second_number = float(input("Enter the second number  "))
+	except ValueError:
+		print("Invalid input, please enter number   ")
+		continue
+	else:
+		break 
+	
+operation = input("Choose the operation (+, -, /, *):  ")
+
 
 if operation == "+":
 	print("The answer is", add(first_number, second_number))
