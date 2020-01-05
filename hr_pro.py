3a63371025e1c8d53220d8b75ab62ddf3ea8b8d4
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-01-05 20:51:06 +0300

    HR Pro

---
+++ hr_pro.py
@@ -0,0 +1,74 @@
+from datetime import date
+
+employees = []
+managers = []
+
+class Employee():
+	def __init__(self, name, age, salary, employment_year):
+		self.name = name
+		self.age = age
+		self.salary = salary
+		self.employment_year = employment_year
+
+	def get_working_years(self):
+		today = date.today()
+		working_years = today.year - self.employment_year
+		return working_years
+
+	def __str__(self):
+		return "name: {}, Age: {}, Salary: {}, Working Years: {}".format(self.name, self.age, self.salary, self.get_working_years())
+		
+class Manager(Employee):
+	def __init__(self, bonus, mbonus):
+		super().__init__(name, age, salary, employment_year)
+		self.bonus = bonus
+		self.mbonus = self.salary * self.bonus
+
+	def __str__(self):
+		return "Name: {}, Age: {}, Salary: {}, Working Years: {}, Bonus: {}".format(self.name, self.age, self.salary, self.get_working_years(), self.mbonus)  
+
+
+options = ["Show Employees", "Show Managers", "Add an Employee", "Add a Manager", "Exit"]
+
+print("Welcome to HR Pro 2019")
+
+stop = "no"
+
+while stop == "no":
+
+	print("Options:")
+
+	for i in options:
+		print (options.index(i) +1, end=" ")
+		print ("", i)
+
+	user_input = int(input("What would you like to do?  "))
+
+	if user_input == 1:
+		for facts in employees:
+			print(facts)
+
+	elif user_input == 2:
+		print(Manager.__str__())
+
+	elif user_input == 3:
+		name = input("Name: ")
+		age = int(input("Age: "))
+		salary = int(input("Salary: "))
+		employment_year = int(input("Employment year: "))
+
+		employees.append(Employee(name, age, salary, employment_year))
+		print("Employee added successfully")
+
+	elif user_input == 4:
+		name = input("Name: ")
+		age = int(input("Age: "))
+		salary = int(input("Salary: "))
+		employment_year = int(input("Employment year: "))
+		bonus = float(input("Bonus Percentge: "))
+
+		Manager(name, age, salary, employment_year, bonus)
+		print("Manager added successfully")
+
+	elif user_input == 5:
+		stop = "yes"
